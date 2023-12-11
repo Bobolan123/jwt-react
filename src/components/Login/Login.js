238e3db90c8b3fae876f8d94cb49f3c3aa52fdcd
Bobolan123 <127038290+Bobolan123@users.noreply.github.com>
2023-12-11 16:24:52 +0700

    responsive for all and basic design

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,10 +3,10 @@ import './Login.scss'
 const UserLogin = (props) => {
     
     return (
-        <div className="login-container mt-3">
+        <div className="login-container">
             <div className="container">
-                <div className="row">
-                    <div className="content-left col-7">
+                <div className="row px-3 px-sm-0">
+                    <div className="content-left col-sm-7 d-none d-sm-block">
                         <div className='brand'>
                             Lan
                         </div>
@@ -14,11 +14,14 @@ const UserLogin = (props) => {
                             Reactjs for life
                         </div>
                     </div>
-                    <div className="content-right green col-5 d-flex flex-column gap-3 py-3">
+                    <div className="content-right green col-12 col-sm-5 d-flex flex-column gap-3 py-3 mg-sm-0 mg-3">
+                        <div className='brand d-sm-none d-block text-center'>
+                            Lan
+                        </div>
                         <input type='text' className='form-control' placeholder='Email or phone number'/>
                         <input type='password' className='form-control' placeholder='Password'/>
                         <button className='btn btn-primary btn-lg'>Login</button>
-                        <span className='text-center'>Forgot your password?</span>
+                        <span className='text-center'><a href='#' className='forget-password'>Forgot your password?</a></span>
                         <hr/>
                         <div className='text-center'>
                             <button className="btn btn-success">Create new account</button>
