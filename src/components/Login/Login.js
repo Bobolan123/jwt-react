9a31315b4c6cdfc2c8475d5c727de3bc3c3a33a9
Bobolan123 <127038290+Bobolan123@users.noreply.github.com>
2023-12-11 14:23:55 +0700

    generate login page left, right and dont content right

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,31 @@
-
+import './Login.scss'
 
 const UserLogin = (props) => {
     
     return (
-        <div className="login-container">
-            Login
+        <div className="login-container mt-3">
+            <div className="container">
+                <div className="row">
+                    <div className="content-left col-7">
+                        <div className='brand'>
+                            Lan
+                        </div>
+                        <div className='detail'>
+                            Reactjs for life
+                        </div>
+                    </div>
+                    <div className="content-right green col-5 d-flex flex-column gap-3 py-3">
+                        <input type='text' className='form-control' placeholder='Email or phone number'/>
+                        <input type='password' className='form-control' placeholder='Password'/>
+                        <button className='btn btn-primary btn-lg'>Login</button>
+                        <span className='text-center'>Forgot your password?</span>
+                        <hr/>
+                        <div className='text-center'>
+                            <button className="btn btn-success">Create new account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
